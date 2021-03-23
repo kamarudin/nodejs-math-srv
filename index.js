34f08e417f3486294d035ea4cfd884262ac31a42
KS <ksamsudi@redhat.com>
2021-03-23 12:49:01 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  ts=Date.now();
+  rand_A = Math.floor(Math.random() * 1000) + 1;
+  rand_B = Math.floor(Math.random() * 10000) + 1;
+  rand_Tot = Math.floor(rand_A * rand_B);
+
+  res.send('Timestamp:'+ ts + ' { A:' + rand_A + ' , B:' + rand_B + ' ; TOTAL (A*B):' + rand_Tot + ' } ');
+})
+
+app.listen(8080, function () {
+  console.log('Listening on port 8080...')
+})
