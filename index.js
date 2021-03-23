69d70a534c578f89c770edc89e6889e607915760
Kamarudin Samsudin <kamarudin@gmail.com>
2021-03-23 22:24:59 +0800

    TEST-0-11
    TEST-0-11

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.get('/', function (req, res) {
   ts=Date.now();
   rand_A = Math.floor(Math.random() * 1000) + 1;
   rand_B = Math.floor(Math.random() * 10000) + 1;
-  rand_Tot = Math.floor(rand_A * rand_B + rand_A / rand_B);
+  rand_Tot = Math.floor((rand_A * (rand_B + rand_A)) / rand_B);
 
-  res.send('TEST-0-10 Timestamp:'+ ts + ' { A:' + rand_A + ' , B:' + rand_B + ' ; TOTAL (A*B*A*B):' + rand_Tot + ' } ');
+  res.send('TEST-0-11 Timestamp:'+ ts + ' { A:' + rand_A + ' , B:' + rand_B + ' ; TOTAL (A*B*A*B):' + rand_Tot + ' } ');
 })
 
 app.listen(8080, function () {
