0a8264f63e58b64911a4fb99f0c355da715ca325
Kamarudin Samsudin <kamarudin@gmail.com>
2021-04-23 14:33:25 +0800

    Update index.js
    Adding Count

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express')
 var app = express()
 var os = require('os')
+my_Count = 0
 
 app.get('/', function (req, res) {
   ts=Date.now();
   rand_A = Math.floor(Math.random() * 1000) + 1;
   rand_B = Math.floor(Math.random() * 10000) + 1;
   rand_Tot = Math.floor(rand_A * rand_B);
+  my_Count = my_Count + 1;
 
   res.setHeader('Content-Type', 'application/json');
-  res.json({Timestamp: ts, A: rand_A, B: rand_B, "TOTAL (A*B)": rand_Tot, Hostname: os.hostname(), Msg: "Demo-0" });
+  res.json({Timestamp: ts, A: rand_A, B: rand_B, "TOTAL (A*B)": rand_Tot, Hostname: os.hostname(), Count: my_Count, Msg: "Demo-0" });
 })
 
 app.listen(8080, function () {
