3cd2fec70d0ddbc6bd6e922f104e149c2ad2fc9a
Kamarudin Samsudin <kamarudin@gmail.com>
2021-04-23 12:08:38 +0800

    Update index.js
    1. Re-based to Demo-0
    2. Change the outout as JSON
    3. Add host hostname info

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var app = express()
+var os = require('os')
 
 app.get('/', function (req, res) {
   ts=Date.now();
   rand_A = Math.floor(Math.random() * 1000) + 1;
   rand_B = Math.floor(Math.random() * 10000) + 1;
-  rand_Tot = Math.floor((rand_A * (rand_B - rand_A)) / rand_B);
+  rand_Tot = Math.floor(rand_A * rand_B);
 
-  res.send('Timestamp:'+ ts + ' { A:' + rand_A + ' , B:' + rand_B + ' ; TOTAL ((A*(B-A))/B):' + rand_Tot + ' } - DEMO-4');
+  res.setHeader('Content-Type', 'application/json');
+  res.json({Timestamp: ts, A: rand_A, B: rand_B, "TOTAL (A*B)": rand_Tot, Hostname: os.hostname(), Msg: "Demo-0" });
 })
 
 app.listen(8080, function () {
